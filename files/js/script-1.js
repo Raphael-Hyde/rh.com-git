adc19299de82ed014ca54164f16b7da85fc7c3e7
Raphael Hyde <raphaelhyd@gmail.com>
2017-09-05 02:11:31 -0700

    roe page updates

--- files/js/script-1.js
+++ files/js/script-1.js
@@ -3,3 +3,19 @@ $("#projects-btn").click(function() {
         scrollTop: $("#portfolio-section").offset().top
     }, 750);
 });
+
+
+/* /// ON:SCROLL PROGRESS BAR /// */
+
+$(window).load(function(){
+  $(window).scroll(function() {
+    var wintop = $(window).scrollTop(), docheight = $('article').height(), winheight = $(window).height();
+    console.log(wintop);
+    var totalScroll = (wintop/(docheight-winheight))*100;
+    console.log("total scroll" + totalScroll);
+    $(".progressBar").css("width",totalScroll+"%");
+  });
+
+});
+
+/* /// END : ON:SCROLL PROGRESS BAR /// */
