f881a9fd7f92e29d9d1b5a7d715aff4aca3190aa
Raphael Hyde <raphaelhyd@gmail.com>
2017-09-05 22:15:38 -0700

    added page:scroll id's

--- files/js/script-1.js
+++ files/js/script-1.js
@@ -19,3 +19,40 @@ $(window).load(function(){
 });
 
 /* /// END : ON:SCROLL PROGRESS BAR /// */
+
+
+$("#roe-phase-1").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#roe-phase1").offset().top
+    }, 750);
+});
+
+$("#roe-phase-2").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#roe-phase2").offset().top
+    }, 750);
+});
+
+$("#roe-phase-3").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#roe-phase3").offset().top
+    }, 750);
+});
+
+$("#roe-phase-4").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#roe-phase4").offset().top
+    }, 750);
+});
+
+$("#roe-phase-5").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#roe-phase5").offset().top
+    }, 750);
+});
+
+$("#roe-timeline-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#roe-timeline").offset().top
+    }, 750);
+});
