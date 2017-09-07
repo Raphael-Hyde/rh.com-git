df6500bdf36837b6bd1fa00e44b1104092354dac
Raphael Hyde <raphaelhyd@gmail.com>
2017-09-06 19:50:27 -0500

    -

--- files/js/script-1.js
+++ files/js/script-1.js
@@ -4,22 +4,7 @@ $("#projects-btn").click(function() {
     }, 750);
 });
 
-
-/* /// ON:SCROLL PROGRESS BAR /// */
-
-$(window).load(function(){
-  $(window).scroll(function() {
-    var wintop = $(window).scrollTop(), docheight = $('article').height(), winheight = $(window).height();
-    console.log(wintop);
-    var totalScroll = (wintop/(docheight-winheight))*100;
-    console.log("total scroll" + totalScroll);
-    $(".progressBar").css("width",totalScroll+"%");
-  });
-
-});
-
-/* /// END : ON:SCROLL PROGRESS BAR /// */
-
+/* SCROLL DOWN SCRIPTS */
 
 $("#roe-phase-1").click(function() {
     $('html, body').animate({
